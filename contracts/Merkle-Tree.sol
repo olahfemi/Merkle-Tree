e48b5818e4e97b4ebbf2f44d15fa7d8e96352a20
Femi Olah <olahfemi@gmail.com>
2022-08-25 08:51:00 +0100

    Initial hardhat commit

---
+++ contracts/Merkle-Tree.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract Verify {
+
+    bytes32 public root;
+
+    constructor(bytes32 _root) {
+        root = _root;
+    }
+
+    function checkInWhitelist(bytes32[] calldata proof, uint64 maxMintAmount) public view returns (bool) {
+        bytes32 leaf = keccak256(abi.encode(msg.sender, maxMintAmount));
+        bool verified = MerkleProof.verify(proof, root, leaf);
+        return verified;
+    }
+}
